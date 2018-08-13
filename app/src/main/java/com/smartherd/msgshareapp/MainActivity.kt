c31ce741d085c88e02b02fd1583fc7844fb84c36
Clevelon.Skerritt <olegallis1@hotmail.com>
2018-08-13 21:27:08 +0100

    Section 2 Video 3

--- app/src/main/java/com/smartherd/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/smartherd/msgshareapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.smartherd.msgshareapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
@@ -18,6 +19,15 @@ class MainActivity : AppCompatActivity() {
             Log.i("MainActivity", "Button was clicked!!!!")
 
             Toast.makeText(this@MainActivity, "Button was clicked!!!", Toast.LENGTH_SHORT).show()
+        }
+
+        btnSendMessage.setOnClickListener {
+            val message : String = etUserMessage.text.toString()
+            Toast.makeText(this@MainActivity, message, Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+
         }
     }
 
